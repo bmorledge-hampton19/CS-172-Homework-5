1f13824ee2cbbcc1cd7e8cb46e2b66489fd8949f
Benjamin Morledge-Hampton <bmorledge-hampton19@my.whitworth.edu>
2016-11-01 16:36:16 -0700

    -Added testing for the IsSorted function.  Everything works alright!

--- Homework 5/Is Sorted/Main.cpp
+++ Homework 5/Is Sorted/Main.cpp
@@ -16,7 +16,7 @@ bool isSorted(const T list[], int size) {
 
 	// Using a for loop, check to see if the array is sorted from least to greatest by ensuring that each value is less than or equal to the next value in the array and return false if it isn't.
 	for (int i = 0; i < (size - 1); i++) {
-		if (!list[i] <= list[i + 1]) return false;
+		if ( !(list[i] <= list[i + 1]) ) return false;
 	}
 
 	return true;
@@ -26,5 +26,30 @@ bool isSorted(const T list[], int size) {
 // Main Function
 
 int main() {
+
+	// Create 3 arrays of size 5 (an int, double, and string array) that are properly sorted.
+	int sortedIntList[5] = { 1,2,3,3,5 };
+	double sortedDoubleList[5] = { 1.5,3,4.5,6,7.5 };
+	string sortedStringList[5] = { "Bad", "Fad", "Had", "Sad", "Tad" };
+
+	// Create 3 arrays of size 5 (an int, double, and string array) that are not properly sorted.
+	int unsortedIntList[5] = { 4,6,2,1,1 };
+	double unsortedDoubleList[5] = { .6, .3, .4, .5, .6 };
+	string unsortedStringList[5] = { "Dad", "Bad", "Rad", "Sad", "Glad" };
+
+	// Test the function with the six above arrays.
+	cout << boolalpha;
+
+	cout << "The first array of integers is properly sorted: " << isSorted(sortedIntList, 5) << endl;
+	cout << "The first array of doubles is properly sorted: " << isSorted(sortedDoubleList, 5) << endl;
+	cout << "The first array of strings is properly sorted: " << isSorted(sortedStringList, 5) << endl << endl;
+
+	cout << "The second array of integers is properly sorted: " << isSorted(unsortedIntList, 5) << endl;
+	cout << "The second array of doubles is properly sorted: " << isSorted(unsortedDoubleList, 5) << endl;
+	cout << "The second array of strings is properly sorted: " << isSorted(unsortedStringList, 5) << endl << endl;
+
+
+	// End function.  Everything is ok!
+	return 0;
 
 }
