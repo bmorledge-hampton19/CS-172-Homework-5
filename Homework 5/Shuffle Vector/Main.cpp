3c145823d57da100fe24671ca002ee1db2a09d88
Benjamin Morledge-Hampton <bmorledge-hampton19@my.whitworth.edu>
2016-11-01 22:01:38 -0700

    -Debugged the Shuffle function so that it works just as intended! -On to the last exercise!

--- Homework 5/Shuffle Vector/Main.cpp
+++ Homework 5/Shuffle Vector/Main.cpp
@@ -16,20 +16,23 @@ using namespace std;
 template <typename T>
 void shuffle(vector<T>& v) {
 
-	int vectorElements = v.size // The number of elements the vector should have by the end of the program.
-	T* shuffledArray = new T[v.size]; // A temporary array to store the vector's values in a shuffled order.
+	int vectorElements = (int)v.size(); // The number of elements the vector should have by the end of the program.
+	T* shuffledArray = new T[v.size()]; // A temporary array to store the vector's values in a shuffled order.
 	int randPos; // A random position of those left in the vector that represents the next index to be moved into the shuffled array.
-
 
 	// Store the vector's values in the temporary array.
 	for (int i = 0; i < vectorElements; i++) {
 		
 		// Get a new random position from the remaining positions in the vector.
-		randPos = rand() % v.size;
+		randPos = rand() % v.size();
 
 		// Move the element at the random position into the shuffled array and erase it from the vector.
-		shuffledArray[i] = v[randompos];
-		v.erase(randPos);
+		shuffledArray[i] = v[randPos];
+		// This for loop moves every element above the one being removed down one position before popping the top position.
+		for (int z = randPos; z < (vectorElements-1-i); z++) {
+			v[z] = v[z + 1];
+		}
+		v.pop_back();
 
 	}
 	
@@ -39,13 +42,41 @@ void shuffle(vector<T>& v) {
 		v.push_back(shuffledArray[i]);
 	}
 
+	// Delete the dynamically allocated array.
+	delete[] shuffledArray;
+
 }
 
 
 //Main function
 int main() {
 
+	// Seed the RNG with the internal clock.
+	srand((unsigned)time(NULL));
 
+	// Create a vector of 10 random integers from 1-50 and shuffle it!  Display the vector before and after the shuffle.
+
+	vector<int> cards(10);
+
+	cout << "The vector before the shuffle: " << endl;
+
+	// Seed and display it!
+	for (int i = 0; i < 10; i++) {
+		cards[i] = rand() % 50 + 1;
+		cout << cards[i] << endl;
+	}
+	cout << endl;
+
+	// Shuffle it!
+	shuffle(cards);
+
+	cout << "The vector after the shuffle: " << endl;
+
+	// Display it again!
+	for (int i = 0; i < 10; i++) {
+		cout << cards[i] << endl;
+	}
+	cout << endl;
 
 	// End function.  Everything is ok!
 	return 0;
